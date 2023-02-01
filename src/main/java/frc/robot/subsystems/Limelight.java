77c3f7589a05b1b81d08b6425b33b530aa7b5705
howard <howard60211@gmail.com>
2023-02-01 14:06:58 +0800

    Fix:Add initial pose2d with limelight and delete update vision measurement.

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -4,38 +4,46 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.networktables.*;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.networktables.DoubleSubscriber;
-import org.photonvision.EstimatedRobotPose;
+
 
 import java.util.Optional;
 
 public class Limelight extends SubsystemBase {
   /** Creates a new Limelight. */
+  final private DoubleArraySubscriber robotToTagSubscriber;
   final private DoubleArraySubscriber robotPoseSubscriber;
+  private final DoubleArraySubscriber robotTeamPoseSubscriber;
   final private DoubleSubscriber tagIDSubscriber;
   final static Limelight INSTANCE = new Limelight();
   public static Limelight getInstance() {
     return INSTANCE;
   }
   public Limelight() {
-    robotPoseSubscriber = NetworkTableInstance.getDefault().getTable("limelight").getDoubleArrayTopic("targetpose_robotspace").subscribe(new double[6]);
-    tagIDSubscriber = NetworkTableInstance.getDefault().getTable("Limelight").getDoubleTopic("tid").subscribe(-1);
+    robotToTagSubscriber = NetworkTableInstance.getDefault().getTable("limelight").getDoubleArrayTopic("targetpose_robotspace").subscribe(new double[6]);
+    robotPoseSubscriber = NetworkTableInstance.getDefault().getTable("limelight").getDoubleArrayTopic("robotpose").subscribe(new double[6]);
+    tagIDSubscriber = NetworkTableInstance.getDefault().getTable("limelight").getDoubleTopic("tid").subscribe(-1);
+    if (DriverStation.getAlliance() == Alliance.Blue) {
+      robotTeamPoseSubscriber = NetworkTableInstance.getDefault().getTable("limelight").getDoubleArrayTopic("botpose_wpiblue").subscribe(new double[6]);
+    } else {
+      robotTeamPoseSubscriber = NetworkTableInstance.getDefault().getTable("limelight").getDoubleArrayTopic("botpose_wpired").subscribe(new double[6]);
+    }
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumberArray("robot Pose from limelight", robotPoseSubscriber.get());
+    SmartDashboard.putNumberArray("robot Pose from limelight", robotToTagSubscriber.get());
   }
 
   public Optional<Transform3d> getAprilTagRelative() {
-    var robotPoseArray = robotPoseSubscriber.get();
+    var robotPoseArray = robotToTagSubscriber.get();
     Optional<Transform3d> CameraToTarget = Optional.empty();
     if (tagIDSubscriber.get() != -1) {
       CameraToTarget = Optional.of(new Transform3d(new Translation3d(robotPoseArray[0], robotPoseArray[1], robotPoseArray[2]), new Rotation3d(robotPoseArray[3], robotPoseArray[4], robotPoseArray[5])));
@@ -43,14 +51,23 @@ public class Limelight extends SubsystemBase {
     return CameraToTarget;
   }
 
-  public Optional<Pair<Pose2d, Double>> getEstimatedGlobalPose() {
-    double currentTime = Timer.getFPGATimestamp();
+  public Optional<Pose2d> getEstimatedGlobalPose() {
     var robotPoseArray = robotPoseSubscriber.get();
     if(tagIDSubscriber.get() == -1) {
       return Optional.empty();
     } else {
       var robotPose2d = new Pose2d(robotPoseArray[0], robotPoseArray[1], new Rotation2d(robotPoseArray[2], robotPoseArray[3]));
-      return Optional.of(new Pair<Pose2d, Double>(robotPose2d, Timer.getFPGATimestamp()));
+      return Optional.of(robotPose2d);
+    }
+  }
+
+  public Optional<Pose2d> getTeamRobotPose() {
+    var robotPoseArray = robotTeamPoseSubscriber.get();
+    if(tagIDSubscriber.get() == -1) {
+      return Optional.empty();
+    } else {
+      var robotPose2d = new Pose2d(robotPoseArray[0], robotPoseArray[1], new Rotation2d(robotPoseArray[2], robotPoseArray[3]));
+      return Optional.of(robotPose2d);
     }
   }
 }
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -14,7 +14,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.RobotMap.DriverPort;
-import frc.robot.subsystems.Limelight;
 
 public class SwerveSubsystem extends SubsystemBase {
 
@@ -55,7 +54,7 @@ public class SwerveSubsystem extends SubsystemBase {
     private final AHRS gyro = new AHRS(SPI.Port.kMXP);
     private final SwerveDrivePoseEstimator SwerveEstimator = new SwerveDrivePoseEstimator(DriveConstants.kDriveKinematics, gyro.getRotation2d(), new SwerveModulePosition[]{
             frontLeft.getPosition(), frontRight.getPosition(), backLeft.getPosition(), backRight.getPosition()
-    }, new Pose2d());
+    }, vision.getEstimatedGlobalPose().orElse(new Pose2d()));
     private final Field2d m_field = new Field2d();
     public SwerveSubsystem() {
         new Thread(() -> {
@@ -103,8 +102,6 @@ public class SwerveSubsystem extends SubsystemBase {
     @Override
     public void periodic() {
         SwerveEstimator.update(getRotation2d(), new SwerveModulePosition[]{frontLeft.getPosition(), frontRight.getPosition(), backLeft.getPosition(), backRight.getPosition()});
-        var gloabalPose = vision.getEstimatedGlobalPose();
-        gloabalPose.ifPresent(pose2dDoublePair -> SwerveEstimator.addVisionMeasurement(pose2dDoublePair.getFirst(), pose2dDoublePair.getSecond()));
         SmartDashboard.putNumber("Robot Heading", getHeading());
         SmartDashboard.putString("Robot Location", getPose().getTranslation().toString());
         SmartDashboard.putData(m_field);
